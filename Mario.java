9d782045ff3655aefb7e9f66d5ecfec353774f89
PlatinumOtter <peaceinchaos13@gmail.com>
2015-05-01 20:16:27 -0500

    reverted changes I accidently merged to master and added in the .gitignore

--- Mario.java
+++ Mario.java
@@ -11,7 +11,6 @@ public class Mario{
 	public static void main(String[] args) throws IOException {
 		
 		Scanner user_input = new Scanner(System.in);
-		PrintWriter printWriter = new PrintWriter("Mario.txt");
 		
 		int n;
 		System.out.print("Enter a number between 0 and 23: ");
@@ -22,64 +21,27 @@ public class Mario{
 			n = Integer.parseInt(user_input.next());
 		}
 		//We should now have an int between 0 and 23 stored as n.
+		for(int i = 0; i < n; i++){
 
-		int outputtype;
-		System.out.print("Enter 1 for console output or 2 for .txt file output: ");
-		outputtype = Integer.parseInt(user_input.next());
+			StringBuilder levelBuilder = new StringBuilder();
 
-		while(outputtype != 1 && outputtype != 2){
-			System.out.print("Please enter 1 for console output or 2 for .txt file output: ");
-			outputtype = Integer.parseInt(user_input.next());
-		}
-		if(outputtype == 1){
+			int spaces;
+			spaces = (n - 1) - i;
 
-			for(int i = 0; i < n; i++){
+			int hashes;
+			hashes = i + 2;
 
-				StringBuilder levelBuilder = new StringBuilder();
-
-				int spaces;
-				spaces = (n - 1) - i;
-
-				int hashes;
-				hashes = i + 2;
-
-				while(spaces > 0){
-					levelBuilder.append(" ");
-					spaces = spaces - 1;
-				}
-
-				while(hashes > 0){
-					levelBuilder.append("#");
-					hashes = hashes - 1;
-				}
-
-				System.out.println(levelBuilder.toString());
+			while(spaces > 0){
+				levelBuilder.append(" ");
+				spaces = spaces - 1;
 			}
-		}else if(outputtype == 2){
 
-			for(int i = 0; i < n; i++){
-
-				StringBuilder levelBuilder = new StringBuilder();
-
-				int spaces;
-				spaces = (n - 1) - i;
-
-				int hashes;
-				hashes = i + 2;
-
-				while(spaces > 0){
-					levelBuilder.append(" ");
-					spaces = spaces - 1;
-				}
-
-				while(hashes > 0){
-					levelBuilder.append("#");
-					hashes = hashes - 1;
-				}
-
-				printWriter.println(levelBuilder.toString());
+			while(hashes > 0){
+				levelBuilder.append("#");
+				hashes = hashes - 1;
 			}
-			printWriter.close();
+
+			System.out.println(levelBuilder.toString());
 		}
 	}
 }
